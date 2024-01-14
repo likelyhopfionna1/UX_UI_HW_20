6d7e0febfe1f30a60eff681330e40d83f4aef61b
likelyhopfionna1 <cecefionnas@gmail.com>
2024-01-14 21:12:47 +1100

    JQuery javascript

---
+++ js/index.js
@@ -0,0 +1,57 @@
+console.log("Your index.js is loaded correctly!")
+
+$(document).ready(function(){
+    $("#button1").hover(function(){
+      $(this).css({"background-color": "#001B2E", "color": "#FFF38B"});
+      }, function(){
+      $(this).css({"background-color": "#075978", "color": "#FFFFFF"});
+    });
+  });
+
+  $(document).ready(function(){
+    $("#button2").hover(function(){
+      $(this).css({"background-color": "#001B2E", "color": "#E5D6EC"});
+      }, function(){
+      $(this).css({"background-color": "#FFFFFF", "color": "#000"});
+    });
+  });
+
+  $(document).ready(function(){
+    $(".hoverCS").hover(function(){
+      $(this).css("color", "#F8E00A");
+      }, function(){
+      $(this).css("color", "#FFFFFF");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#hoverAllprojects").hover(function(){
+      $(this).css("color", "#9E7B9B");
+      }, function(){
+      $(this).css("color", "#000000");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#buttonUp").hover(function(){
+      $(this).css("background-color", "#E5D6EC");
+      }, function(){
+      $(this).css("background-color", "#075978");
+    });
+  });
+
+  $("#buttonUp").click (function () {
+    $("html, head"). animate ({scrollTop: 0}, 1000);
+  });
+
+
+
+//   $(".inline-block").hover(function(){
+//     $(this).css("color", "#9E7B9B");
+// }, function(){
+// $(this).css("color", "#00000");
+// });
+
+
+
+
